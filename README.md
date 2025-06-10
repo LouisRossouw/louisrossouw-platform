ed41e016ac87d023e1917789475efa4e09dfe8f4
Louis <lourou99@gmail.com>
2025-06-10 11:36:17 +0200

    -

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# Experimental portfolio platform that will be split up as:
+
+- Landing page portfolio, experimental with threejs experiences that redirects to dev / anim portfolio
+- Devepoment portfolio
+- Animation portfolio
+- Shop webapp maybe?
+- API to support above apps + monetize my small chrome extensions and other small tools in the film + VFX space.
+
+To self host on a VPS with coolify under sub domains, --- No idea how well it will work, but there should be little to no traffic to these apps.
+
 # shadcn/ui monorepo template
 
 This template is for creating a monorepo with shadcn/ui.
--- apps/portfolio/src/App.tsx
+++ apps/portfolio/src/App.tsx
@@ -1,39 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
 import { Button } from "@repo/ui/components/button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="bg-yellow-500">
-        <Button variant={"destructive"} onClick={() => console.log("!!!")}>
-          TEST
-        </Button>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex w-full h-screen items-center justify-center">
+      <div className="flex items-center justify-center w-1/3 h-1/3 border rounded-lg">
+        <div className="grid gap-4">
+          <div>Portfolio landing page</div>
+          <Button variant={"destructive"} onClick={() => alert("test")}>
+            Test Button
+          </Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
